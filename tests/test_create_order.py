26fe2316a74b9dbd04ac27b16768a9ad15c55997
Daniil Gordienko <danila.gordi@yandex.ru>
2024-03-07 00:23:33 +0300

    first commit

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -10,27 +10,27 @@ class TestApiCreateOrder:
     @allure.title('Тест создания заказа для авторизованного пользователя')
     def test_auth_user_create_order_successful(self, token):
         user_token = token
-        payload = Ingredients.INGREDIENTS
-        response = requests.post(EndpointsUrl.ORDER, headers={'Authorization': user_token}, data=payload)
+        data = Ingredients.INGREDIENTS
+        response = requests.post(EndpointsUrl.ORDER, headers={'Authorization': user_token}, json=data)
         assert response.status_code == 200 and response.json()['success']
 
     @allure.title('Тест создания заказа с ингридиентами без авторизации')
     def test_create_order_with_ingredients_user_no_auth_successful(self):
-        payload = Ingredients.INGREDIENTS
-        response = requests.post(EndpointsUrl.ORDER, data=payload)
+        data = Ingredients.INGREDIENTS
+        response = requests.post(EndpointsUrl.ORDER, json=data)
         assert response.status_code == 200 and response.json()['success']
 
     @allure.title('Тест создания заказа без ингридиентов')
     def test_create_order_without_ingredients_error(self):
-        payload = Ingredients.WITHOUT_INGREDIENTS
-        response = requests.post(EndpointsUrl.ORDER, data=payload)
+        data = Ingredients.WITHOUT_INGREDIENTS
+        response = requests.post(EndpointsUrl.ORDER, json=data)
         assert response.status_code == 400
         assert response.json()['message'] == ErrorMessage.ERROR_INGREDIENT
 
     @allure.title('Тест создания заказа с некорректными ингридиентами')
     def test_create_order_bad_ingredients_error(self):
-        payload = Ingredients.INCORRECT_INGREDIENTS
-        response = requests.post(EndpointsUrl.ORDER, data=payload)
+        data = Ingredients.INCORRECT_INGREDIENTS
+        response = requests.post(EndpointsUrl.ORDER, json=data)
         assert response.status_code == 500
         assert ErrorMessage.SERVER_ERROR in response.text
 
